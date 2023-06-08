730846b408f1f91914f602c450b1800fa8793a0b
郭方超 <guofangchao@idongwo.com>
2023-06-08 21:20:36 +0800

    增加守护进程

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import axios from 'axios';
 
 const app = express();
-const port = 18000;
+const port = 18000 || process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
